1e74921b8e6c63c23f2e93f3d4b2c79f8651c8d0
Ebrahim mamdouh <138860098+ebrahim-mamdoh@users.noreply.github.com>
2024-10-22 13:45:30 +0300

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './About.module.css'
+
+export default function About() {
+  return<>
+  <h2>About</h2>
+  </>
+}
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './Home.module.css'
+
+export default function Home() {
+  return<>
+  <h2>Home</h2>
+  </>
+}
